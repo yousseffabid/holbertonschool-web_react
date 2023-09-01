cae8524d40230f7cf6a60fed4889f307979e40ac
yousseffabid <abidyoussef58@gmail.com>
2023-09-01 18:02:07 +0100

    substring

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,10 @@
+#ifndef _SUBSTRING_H_
+#define _SUBSTRING_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif /*_SUBSTRING_H_*/
